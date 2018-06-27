c31938e638acd9e53591c2c07b11b49f578107cd
yu <yubei874721995@163.com>
2018-06-27 11:53:26 +0800

    '增加权限设置'

---
+++ C_TestCase/test_Detail.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 13:32
+# @Author  : Carewn
+# @Software: PyCharm
+
+import requests
+import unittest
+from Public.get_api import GetApi
+from Public.logger import Logger
+from Public.Get_login_token import Get_Login
+
+mylog = Logger(logger='webLog').getlog()
+class Detail(unittest.TestCase):
+
+    def setUp(self):
+        self.detailUrl = GetApi('test_host','Detail').main()
+        self.token = Get_Login().get_test_token()
+
+    def tearDown(self):
+        pass
+
+    def test_DetailCheck(self):
+        '''web端权限管理验证'''
+        data = {
+            'psotID':1
+        }
+        headers = {
+            'Authorization':self.token
+        }
+        r = requests.post(self.detailUrl,data=data,headers=headers)
+        failDetail = []
+        for i in r.json()['data']:
+            # print (i)
+            for e in i['subRoleFucList']:
+                if e['pid'] != i['functionId']:
+                    failDetail.append(e['funcName'])
+                for s in e['subRoleFucList']:
+                    if s['pid'] != e['functionId']:
+                        failDetail.append(s['funcName'])
+        print (failDetail)
+        print (r.json())
+        assert r.json()['msg'] == '操作成功'
+        mylog.info('权限读取成功')
+
+if __name__ == '__main__':
+    unittest.main()
